74ab3ae0c35f315da8db904a954b367c32975265
Civix <dexterdavismvp@gmail.com>
2025-07-31 12:44:42 -0500

    Add files via upload

---
+++ Fly.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using Utilla;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using GorillaLocomotion;
+using GorillaExtensions;
+
+
+
+
+namespace Fly
+{
+    [BepInPlugin("com.gorillatag.Fly.civix", "Fly", "1.0.0")]
+    class Flymod : BaseUnityPlugin
+    {
+        bool inRoom => NetworkSystem.Instance.InRoom && NetworkSystem.Instance.GameModeString.Contains("MODDED");
+        void Update()
+        {
+            if (inRoom)
+            {
+                if (ControllerInputPoller.instance.rightControllerPrimaryButton)
+                {
+                    GorillaLocomotion.GTPlayer.Instance.transform.position += (GorillaLocomotion.GTPlayer.Instance.headCollider.transform.forward) * Time.deltaTime * 15;
+                    GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                }
+
+                else
+                {
+                    if (ControllerInputPoller.instance.leftControllerPrimaryButton)
+                    {
+                        GorillaLocomotion.GTPlayer.Instance.transform.position -= (GorillaLocomotion.GTPlayer.Instance.headCollider.transform.forward) * Time.deltaTime * 15;
+                        GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    }
+
+                    else
+                    {
+                        GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
